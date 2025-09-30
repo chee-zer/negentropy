a1d5534b6dbb5ef557da59ae96a7823a73ceabd0
Subhransu Jena <subunc5@gmail.com>
2025-09-30 22:02:56 +0530

    resume project

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
+import "github.com/charmbracelet/bubbles/key"
+
 type model struct {
 	tabs  Tabs
-	timer Timer
+	timer Model
 	// bar Bar
+}
+
+// global keymap
+// timer - start/stop(space, enter)
+// switch timer while timer not running(tab, l)
+//
+// exit (q)
+type keymap struct {
+	startStop   key.Binding
+	switchTimer key.Binding
+	exit        key.Binding
 }
 
 func main() {
--- timer.go
+++ timer.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/charmbracelet/bubbles/timer"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
 
-type Timer struct {
-	timer timer.Model
+type Model struct {
+	Label   string
+	Running bool
+	ID      int
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+// TODO: for Update
+// [] handle play/pause
+// [] reset(with prompt-reset just adds into entropy)
+// [] complete task (later)
+
+// actually i cannot reuse the timer bubble, its a countdown timer and i need to make a normal timer. Whatever thats called
+// []cmd:
+// []start
+// [] stop
+// [] reset
+
+// [] msg:
+// [] startstop
+// [] tick
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return nil, nil
+}
+
+func (m Model) View() string {
+	return ""
 }
