ddabb665035e409bf7063434f454fbbc593bccaa
Subhransu Jena <subunc5@gmail.com>
2025-11-09 13:40:33 +0530

    basic skele

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
-import "github.com/charmbracelet/bubbles/key"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	db "github.com/chee-zer/negentropy/database/sqlc"
+)
+
+// type model struct {
+// 	tabs  Tabs
+// 	timer Model
+// 	// bar Bar
+// }
 
 type model struct {
-	tabs  Tabs
-	timer Model
-	// bar Bar
+	db           *db.Queries
+	tasks        []db.Task
+	activeTaskId int
+	timerRunning bool
+	//timer stopwatch.Model
 }
 
 // global keymap
@@ -14,11 +32,66 @@ type model struct {
 //
 // exit (q)
 type keymap struct {
-	startStop   key.Binding
-	switchTimer key.Binding
-	exit        key.Binding
+	startStopTimer key.Binding
+	switchTimer    key.Binding
+	exit           key.Binding
+	goRight        key.Binding
+	goLeft         key.Binding
+	deleteTask     key.Binding
+	createTask     key.Binding
+	resetTimer     key.Binding
+}
+
+func NewModel(queries *db.Queries) model {
+	tasks, err := queries.GetTasks(context.Background())
+	if err != nil {
+		log.Fatalf("couldn't not load tasks: %v", err)
+	}
+	return model{
+		db:    queries,
+		tasks: tasks,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	/*
+		keyMsg handling
+		if no task is created, prompt user to create new task, only 'n' is allowed
+		 check if timer running, then split the keyMsgs
+		if timer running:
+		[] - only start stop allowed
+		[] - reset only when timer stopped, so display a message showing so
+		[] - same with navigation tasks(a, d, h, l, left, right), show message
+		if timer not running:
+		[] - navigation (a, d, h, l, left, right) for navigating tabs
+		[] - n - opens a text prompt and creates new task
+		[] - x - open a prompt to confirm deletion of task. the records wont be deleted, and the if the task with same name is created, it gets restored
+		[] - start stop also allowed
+		[] - quit (q)
+	*/
+	return nil, nil
+}
+
+func (m model) View() string {
+	return ""
 }
 
 func main() {
+	sqlitedb, err := sql.Open("sqlite3", "./database/appdb.sqlite")
+	if err != nil {
+		log.Fatalf("Couldn't connect to db: %v", err.Error())
+	}
+	defer sqlitedb.Close()
+
+	queries := db.New(sqlitedb)
+	p := tea.NewProgram(NewModel(queries))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("could'nt run program: %v", err)
+		os.Exit(1)
+	}
 
 }
--- tabs.go
+++ tabs.go
@@ -1,6 +1,26 @@
 package main
 
-type Tabs struct {
-	activeTab int
-	labels    []string
+import tea "github.com/charmbracelet/bubbletea"
+
+type TabModel struct {
+	activeTab         int
+	tasksWithProgress map[int]map[string]float32
+}
+
+//functionality needed:
+// 1. Display tabs (duh)
+// 2. switch tabs when timer not running
+// 3. Create new tabs
+// 4. Show completion percentages
+
+func (m TabModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m TabModel) Update(msg tea.Msg) (tea.Cmd, tea.Model) {
+	return nil, nil
+}
+
+func (m TabModel) View() string {
+	return ""
 }
